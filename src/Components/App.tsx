87573c71e5e2e81ebcca3cf89525d0262e423006
SamB133 <64711289+SamB133@users.noreply.github.com>
2021-03-17 04:43:04 -0400

    Added Single Item Delete Button, Optimized for Use on Mobile Phone, Moved Component Files to Separate Folder
    Added a button to delete a single item by swiping right to show the 'Delete' button and left to re-hide the button, optimized for use on mobile phone with scaling and CSS improvements, moved component files to a separate 'Components' folder for organization.

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,9 +1,10 @@
 import { useState, useRef, useEffect, FC } from 'react';
-import ItemList, { ListItem } from './ItemList';
+import ListItem, { ListItemType } from './ListItem';
+import List from './List';
 import produce from 'immer';
 
 const App: FC = () => {
-	const [listItems, setListItems] = useState<ListItem[]>([]);
+	const [listItems, setListItems] = useState<ListItemType[]>([]);
 	const [ordered, setOrdered] = useState(false);
 	const itemRef = useRef<HTMLInputElement>(null);
 	const addButtonRef = useRef<HTMLButtonElement>(null);
@@ -13,7 +14,7 @@ const App: FC = () => {
 	useEffect(() => {
 		if (localStorage.getItem(localStorageItemsKey) == null) { return }
 		else {
-			const storedListItems: ListItem[] = JSON.parse(localStorage.getItem(localStorageItemsKey) as string);
+			const storedListItems: ListItemType[] = JSON.parse(localStorage.getItem(localStorageItemsKey) as string);
 			if (storedListItems) setListItems(storedListItems);
 		}
 		if (localStorage.getItem(localStorageOrderedKey) == null) { return }
@@ -48,14 +49,28 @@ const App: FC = () => {
 		}
 	};
 
+	function complete(index: number) {
+		setListItems(produce(listItems, newListItems => {
+			newListItems[index].complete = !newListItems[index].complete;
+		}))
+	};
+
+	function deleteItem(id: number) {
+		setListItems(listItems.filter(item => item.id !== id));
+	}
+
 	return (
 		<div className='centered'>
-			<div><button onClick={() => setOrdered(!ordered)} >Change List Type (Ordered or Unordered)</button></div>
-			<div><input type="text" ref={itemRef} onKeyPress={(e) => { if (e.key === 'Enter') addButtonRef.current?.click(); }} /></div>
-			<button style={{ color: 'green' }} onClick={addItem} ref={addButtonRef} >Add Item to List</button>
-			<button style={{ color: 'red' }} onClick={() => setListItems(listItems.filter(item => !item.complete))} >Delete Completed Items</button>
-			<div>You have {listItems.length} items in your list</div>
-			<ItemList listItems={listItems} ordered={ordered} setListItems={setListItems} />
+			<div><button className='buttonsInput' onClick={() => setOrdered(!ordered)} >Change List Type (Ordered or Unordered)</button></div>
+			<div><input className='buttonsInput' type="text" ref={itemRef} onKeyPress={(e) => { if (e.key === 'Enter') addButtonRef.current?.click(); }} /></div>
+			<button className='buttonsInput' style={{ color: 'green' }} onClick={addItem} ref={addButtonRef} >Add Item to List</button>
+			<button className='buttonsInput' style={{ color: 'red' }} onClick={() => setListItems(listItems.filter(item => !item.complete))} >Delete Completed Items</button>
+			<div className='text' >You have {listItems.length} items in your list</div>
+			<List ordered={ordered}>
+				{listItems.map((item, index) => (
+					<ListItem key={item.id} item={item} onClickItem={() => complete(index)} onDeleteItem={() => deleteItem(item.id)} />
+				))}
+			</List>
 		</div>
 	);
 };
